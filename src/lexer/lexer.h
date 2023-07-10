dc90a68b143219751cf60fd3b508992368327e56
shecannotsee <shecannotsee@163.com>
2023-07-10 15:11:48 +0800

    [feat] Add framework.

---
+++ src/lexer/lexer.h
@@ -0,0 +1,16 @@
+//
+// Created by shecannotsee on 23-7-10.
+//
+
+#ifndef SHE_INI_PLUS_SRC_LEXER_LEXER_H_
+#define SHE_INI_PLUS_SRC_LEXER_LEXER_H_
+
+namespace she_ini_plus {
+
+class lexer {
+
+};
+
+} // she_ini_plus
+
+#endif //SHE_INI_PLUS_SRC_LEXER_LEXER_H_
