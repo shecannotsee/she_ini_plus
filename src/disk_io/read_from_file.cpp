7a18d19b9c5bb24cfc8626b69dd761b9a9af9f45
shecannotsee <shecannotsee@163.com>
2023-07-27 16:44:01 +0800

    [feat] Add interface implementation.

--- src/disk_io/read_from_file.cpp
+++ src/disk_io/read_from_file.cpp
@@ -3,6 +3,23 @@
 //
 
 #include "read_from_file.h"
+#include <stdexcept>
 
-namespace she_ini_plus {
-} // she_ini_plus
+she_ini_plus::read_from_file::read_from_file(std::string path) noexcept {
+  file_handle_ = fopen(path.c_str(),"r");
+  if (file_handle_ == NULL) {
+    throw std::runtime_error("Failed to open file."); // 抛出运行时错误异常
+  }
+}
+
+she_ini_plus::read_from_file::~read_from_file() {
+  fclose(file_handle_);
+}
+
+std::tuple<bool, char> she_ini_plus::read_from_file::get() {
+  char c;
+  while ((c = fgetc(file_handle_)) != EOF) {
+    return std::make_pair(true,c);
+  }
+  return std::make_pair(false,'\0');
+}
