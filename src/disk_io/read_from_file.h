a3b71c736042d0f9c3d06000c7752915fe1844b3
shecannotsee <shecannotsee@163.com>
2023-07-27 16:52:28 +0800

    [docs] Add function description

--- src/disk_io/read_from_file.h
+++ src/disk_io/read_from_file.h
@@ -10,19 +10,40 @@
 
 namespace she_ini_plus {
 
+/**
+ * @brief
+ * @example
+ */
 class read_from_file {
  public:
   read_from_file() = delete;
-  read_from_file(std::string path) noexcept;
+  read_from_file(const std::string& path) noexcept;
   ~read_from_file();
+
  private:
-  FILE* file_handle_;
+  FILE* file_handle_; ///< file handle.
+
  public:
+  /**
+   * @brief This function is used to retrieve a single character in a file.
+   * @return bool:is there a valid character.char:valid character
+   */
   std::tuple<bool,char> get();
 
+  /**
+   * @brief Obtain bool type in tuple.
+   * @param ret read_from_file::get()
+   * @return Returns type bool in tuple
+   */
   static bool data_is_valid(std::tuple<bool,char> ret) {
     return std::get<0>(ret);
   };
+
+  /**
+   * @brief Obtain char type in tuple.
+   * @param ret read_from_file::get()
+   * @return Returns type char in tuple
+   */
   static char valid_data(std::tuple<bool,char> ret) {
     return std::get<1>(ret);
   }
