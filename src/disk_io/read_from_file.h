9302d1de5fdcd93ff38cda6429cacfd85543b7f1
shecannotsee <shecannotsee@163.com>
2023-07-27 16:44:18 +0800

    [feat] Add easy-to-use interface.

--- src/disk_io/read_from_file.h
+++ src/disk_io/read_from_file.h
@@ -6,18 +6,26 @@
 #define SHE_INI_PLUS_SRC_DISK_IO_READ_FROM_FILE_H_
 
 #include <string>
-#include <memory>
+#include <tuple>
 
 namespace she_ini_plus {
 
 class read_from_file {
  public:
-  read_from_file();
+  read_from_file() = delete;
+  read_from_file(std::string path) noexcept;
   ~read_from_file();
  private:
   FILE* file_handle_;
  public:
   std::tuple<bool,char> get();
+
+  static bool data_is_valid(std::tuple<bool,char> ret) {
+    return std::get<0>(ret);
+  };
+  static char valid_data(std::tuple<bool,char> ret) {
+    return std::get<1>(ret);
+  }
 
 };
 
