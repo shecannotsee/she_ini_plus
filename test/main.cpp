7e021ff7f9809bd5dc32c14b8de6c8da48be0222
shecannotsee <shecannotsee@163.com>
2023-07-27 16:58:40 +0800

    [test] Add test switch.

--- test/main.cpp
+++ test/main.cpp
@@ -9,14 +9,21 @@
 
 int main() {
   std::cout << YELLOW_COLOR << "Start test " << RESET_COLOR << std::endl;
-  t1_disk_io_interface::main();
-
-
-  testing::InitGoogleTest();
-
-  if (RUN_ALL_TESTS()==0) {
-    std::cout << GREEN_COLOR << "Pass the test." << RESET_COLOR << std::endl;
-  } else {
-    std::cout << RED_COLOR << "Failed the test." << RESET_COLOR << std::endl;
+  constexpr bool easy_test  = false;
+  constexpr bool test_suite = true;
+  if(easy_test) {
+    t1_disk_io_interface::main();
   }
+
+  if(test_suite) {
+    testing::InitGoogleTest();
+
+    if (RUN_ALL_TESTS() == 0) {
+      std::cout << GREEN_COLOR << "Pass the test." << RESET_COLOR << std::endl;
+    } else {
+      std::cout << RED_COLOR << "Failed the test." << RESET_COLOR << std::endl;
+    }
+  }
+
+  return 0;
 };
