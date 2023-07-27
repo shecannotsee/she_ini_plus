d5a5a741ec2da8a5dec9d0bebf80d0545ae587fa
shecannotsee <shecannotsee@163.com>
2023-07-27 16:08:42 +0800

    [test] Add class : write_to_file

---
+++ src/disk_io/write_to_file.cpp
@@ -0,0 +1,8 @@
+//
+// Created by shecannotsee on 23-7-27.
+//
+
+#include "write_to_file.h"
+
+namespace she_ini_plus {
+} // she_ini_plus
---
+++ src/disk_io/write_to_file.h
@@ -0,0 +1,16 @@
+//
+// Created by shecannotsee on 23-7-27.
+//
+
+#ifndef SHE_INI_PLUS_SRC_DISK_IO_WRITE_TO_FILE_H_
+#define SHE_INI_PLUS_SRC_DISK_IO_WRITE_TO_FILE_H_
+
+namespace she_ini_plus {
+
+class write_to_file {
+
+};
+
+} // she_ini_plus
+
+#endif //SHE_INI_PLUS_SRC_DISK_IO_WRITE_TO_FILE_H_
