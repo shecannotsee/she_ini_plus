5093a94070a6dc14df1077f50b068f6894361d7f
shecannotsee <shecannotsee@163.com>
2023-07-27 16:08:06 +0800

    [test] Add class :character_scanning

---
+++ src/lexer/character_scanning.cpp
@@ -0,0 +1,8 @@
+//
+// Created by shecannotsee on 23-7-27.
+//
+
+#include "character_scanning.h"
+
+namespace she_ini_plus {
+} // she_ini_plus
---
+++ src/lexer/character_scanning.h
@@ -0,0 +1,16 @@
+//
+// Created by shecannotsee on 23-7-27.
+//
+
+#ifndef SHE_INI_PLUS_SRC_LEXER_CHARACTER_SCANNING_H_
+#define SHE_INI_PLUS_SRC_LEXER_CHARACTER_SCANNING_H_
+
+namespace she_ini_plus {
+
+class character_scanning {
+
+};
+
+} // she_ini_plus
+
+#endif //SHE_INI_PLUS_SRC_LEXER_CHARACTER_SCANNING_H_
