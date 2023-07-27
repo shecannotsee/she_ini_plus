bc638479c6ec1331800b187fdb5ef65112b47e71
shecannotsee <shecannotsee@163.com>
2023-07-27 18:04:34 +0800

    [test] Add symbol table.

---
+++ src/lexer/symbol_table.h
@@ -0,0 +1,30 @@
+//
+// Created by shecannotsee on 23-7-27.
+//
+
+#ifndef SHE_INI_PLUS_SRC_LEXER_SYMBOL_TABLE_H_
+#define SHE_INI_PLUS_SRC_LEXER_SYMBOL_TABLE_H_
+
+#include <string>
+#include <tuple>
+
+namespace she_ini_plus {
+
+enum class SYMBOL_TYPE : int {
+  COMMENTS,     ///< Comments,like "//" in c/c++.
+  DELIMITER,    ///< Delimiter,like ';' in c/c++.
+  IDENTIFIERS,  ///< Identifiers,like 'a' in "int a","void a()" in c/c++.
+  EXPRESSION,   ///< Just expression.
+  CONSTANTS,    ///< Constants,like '5' in "int a= 5" in c/c++
+  OPERATOR,     ///< Operator,like '=','+' int c/c++
+};
+
+std::tuple<SYMBOL_TYPE,std::string> symbol_table[] {
+    {SYMBOL_TYPE::COMMENTS,     "//"},
+    {SYMBOL_TYPE::DELIMITER,    ";"},
+    {SYMBOL_TYPE::OPERATOR,     "="}
+};
+
+} // she_ini_plus
+
+#endif //SHE_INI_PLUS_SRC_LEXER_SYMBOL_TABLE_H_
