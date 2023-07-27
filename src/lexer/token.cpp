341348c69bd821c77546a6381d196a8007876ef6
shecannotsee <shecannotsee@163.com>
2023-07-27 16:08:30 +0800

    [test] Add class : token

---
+++ src/lexer/token.cpp
@@ -0,0 +1,8 @@
+//
+// Created by shecannotsee on 23-7-27.
+//
+
+#include "token.h"
+
+namespace she_ini_plus {
+} // she_ini_plus
---
+++ src/lexer/token.h
@@ -0,0 +1,16 @@
+//
+// Created by shecannotsee on 23-7-27.
+//
+
+#ifndef SHE_INI_PLUS_SRC_LEXER_TOKEN_H_
+#define SHE_INI_PLUS_SRC_LEXER_TOKEN_H_
+
+namespace she_ini_plus {
+
+class token {
+
+};
+
+} // she_ini_plus
+
+#endif //SHE_INI_PLUS_SRC_LEXER_TOKEN_H_
