ba32a960782b144d840a3ec4f7caad2813b99261
shecannotsee <shecannotsee@163.com>
2023-08-01 17:47:21 +0800

    [feat] Add buffer class.

--- src/lexer/character_scanning.cpp
+++ src/lexer/character_scanning.cpp
@@ -4,5 +4,23 @@
 
 #include "character_scanning.h"
 
-namespace she_ini_plus {
-} // she_ini_plus
+she_ini_plus::character_scanning::character_scanning(const std::string &file_path, const int& size) noexcept
+    : file_(file_path),
+      buffer_size_(size) {
+  // ...
+}
+
+std::vector<char> she_ini_plus::character_scanning::getchar() {
+  std::vector<char> buffer = std::move(std::vector<char>(buffer_size_));
+
+  for (int i = 0; i < buffer_size_; ++i) {
+    auto get_from_file = file_.get();
+    if (file_handle::data_is_valid(get_from_file)) {
+      buffer.push_back(file_handle::valid_data(get_from_file));
+    } else {
+      buffer.resize(i);
+    }
+  }
+
+  return buffer;
+}
--- src/lexer/character_scanning.h
+++ src/lexer/character_scanning.h
@@ -5,10 +5,37 @@
 #ifndef SHE_INI_PLUS_SRC_LEXER_CHARACTER_SCANNING_H_
 #define SHE_INI_PLUS_SRC_LEXER_CHARACTER_SCANNING_H_
 
+#include <disk_io/read_from_file.h>
+#include <vector>
+
 namespace she_ini_plus {
 
 class character_scanning {
+ public:
+  character_scanning() = delete;
+  /**
+   * @brief constructor
+   * @param file_path Set the path for reading files
+   * @param size Set the size of the read buffer
+   */
+  explicit character_scanning(const std::string& file_path,const int& size = 1) noexcept;
+  ~character_scanning() = default;
 
+ private:
+  int buffer_size_{1};
+  using file_handle = read_from_file;
+  file_handle file_;
+
+ public:
+  /**
+   * @brief set the size of the read buffer
+   * @param size
+   */
+  void set_buffer_size(int size) {
+    buffer_size_ = size;
+  }
+
+  std::vector<char> getchar();
 };
 
 } // she_ini_plus
