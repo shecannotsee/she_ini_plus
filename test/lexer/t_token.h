54bb91e9637f736a046b327ab7b058562e77d136
shecannotsee <shecannotsee@163.com>
2023-08-04 18:17:53 +0800

    [test] Add matching and token to the test.

---
+++ test/lexer/t_token.h
@@ -0,0 +1,10 @@
+//
+// Created by shecannotsee on 23-8-4.
+//
+
+#ifndef SHE_INI_PLUS_TEST_LEXER_T_TOKEN_H_
+#define SHE_INI_PLUS_TEST_LEXER_T_TOKEN_H_
+
+#include <lexer/token.h>
+
+#endif //SHE_INI_PLUS_TEST_LEXER_T_TOKEN_H_
