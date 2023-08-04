d43cb67e0c976b5a50840cde5f46b3a022721de6
shecannotsee <shecannotsee@163.com>
2023-08-04 18:18:58 +0800

    [feat] Add error handling of lexical analyzer.

---
+++ src/lexer/lexer_error_handling.cpp
@@ -0,0 +1,8 @@
+//
+// Created by shecannotsee on 23-8-4.
+//
+
+#include "lexer_error_handling.h"
+
+namespace she_ini_plus {
+} // she_ini_plus
---
+++ src/lexer/lexer_error_handling.h
@@ -0,0 +1,16 @@
+//
+// Created by shecannotsee on 23-8-4.
+//
+
+#ifndef SHE_INI_PLUS_SRC_LEXER_LEXER_ERROR_HANDLING_H_
+#define SHE_INI_PLUS_SRC_LEXER_LEXER_ERROR_HANDLING_H_
+
+namespace she_ini_plus {
+
+class lexer_error_handling {
+
+};
+
+} // she_ini_plus
+
+#endif //SHE_INI_PLUS_SRC_LEXER_LEXER_ERROR_HANDLING_H_
