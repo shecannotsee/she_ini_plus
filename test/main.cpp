26b7e3cccc26c033f6e232aa7b94d8896acb71dc
shecannotsee <shecannotsee@163.com>
2023-08-04 18:17:56 +0800

    [test] Add matching and token to the test.

--- test/main.cpp
+++ test/main.cpp
@@ -4,8 +4,12 @@
 #include <iostream>
 #include "global_test_set.h"
 #include "test_example.h"
+// test_suite
 #include "disk_io/t_read_from_file.h"
 #include "lexer/t_character_scanning.h"
+#include "lexer/t_matching.h"
+#include "lexer/t_token.h"
+// easy_test
 #include "t1_disk_io_interface.h"
 
 int main() {
