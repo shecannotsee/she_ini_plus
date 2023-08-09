13dd588a4b901061e0834420db6d989be66d9577
shecannotsee <shecannotsee@163.com>
2023-08-09 12:00:32 +0800

    [test] Add test case for class matching.

--- test/lexer/t_matching.h
+++ test/lexer/t_matching.h
@@ -6,5 +6,30 @@
 #define SHE_INI_PLUS_TEST_LEXER_T_MATCHING_H_
 
 #include <lexer/matching.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+TEST(a,b) {
+  using matching = she_ini_plus::matching;
+  using TOKEN_TYPE = she_ini_plus::TOKEN_TYPE;
+  using SYMBOL_TYPE = she_ini_plus::SYMBOL_TYPE;
+  using token = matching::token;
+  matching matching_tool;
+
+  std::vector<char> character_stream;
+  auto string_to_vector = [&character_stream](std::string str){
+    std::copy(str.begin(), str.end(), std::back_inserter(character_stream));
+  };
+  token return_token{TOKEN_TYPE::UNKNOWN,""};
+
+  string_to_vector("#123");
+  return_token = matching_tool.token_generation(character_stream);
+  EXPECT_EQ(matching::get_token_type(return_token),TOKEN_TYPE::CONSTANTS);
+  EXPECT_EQ(matching::get_token_str(return_token),"123");
+
+
+
+}
 
 #endif //SHE_INI_PLUS_TEST_LEXER_T_MATCHING_H_
